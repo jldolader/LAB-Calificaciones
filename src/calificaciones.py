b9daef6d96760fa429f38a56d81607d20fe33418
jldolader <juanlumsevilla@gmail.com>
2025-01-11 21:24:25 +0100

    ✅ Ejercicio 1: c)

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -4,3 +4,10 @@ def nota_teoria(n1: float|None, n2:float|None) -> float:
 def nota_cuatrimestre(ntt: tuple[float|None, float|None], np: float|None) -> float:
     nt = nota_teoria(ntt[0], ntt[1])
     return (0.2 * nt + 0.8 * (np if type(np) is float else 0)) if nt >= 4 else 0
+
+def nota_continua(nt: tuple[float|None, float|None, float|None, float|None],
+                  np: tuple[float|None, float|None]) -> float:
+    nc1 = nota_cuatrimestre((nt[0], nt[1]), np[0])
+    nc2 = nota_cuatrimestre((nt[2], nt[3]), np[1])
+    
+    return (nc1 + nc2) / 2 if nc1 >= 4 and nc2 >= 4 else min(4, (nc1 + nc2) / 2)
--- src/calificaciones_test.py
+++ src/calificaciones_test.py
@@ -14,8 +14,14 @@ def test_nota_cuatrimestre():
     print(nota_cuatrimestre((4.0, 3.0), None))
     print(nota_cuatrimestre((None, 3.0), None))
     print(nota_cuatrimestre((9.0, None), 4.5))
-    
+
+def test_nota_continua():
+    print(nota_continua((9.6, 9.9, 10.0, 9.8), (9.7, 9.5)))
+    print(nota_continua((4.4, 4.9, 5.1, 4.7), (4.6, 4.8)))
+    print(nota_continua((4.0, 6.0, 4.0, 3.0), (5.0, None)))
+    print(nota_continua((9.0, None, 4.0, 3.0), (4.5, None)))
     
 if __name__ == '__main__':
     #test_nota_teoria()
-    test_nota_cuatrimestre()
+    #test_nota_cuatrimestre()
+    test_nota_continua()
