d90f6b402a928e72cb14cd9e71458763a8d5728f
jldolader <juanlumsevilla@gmail.com>
2025-01-11 21:04:58 +0100

    ✅ Ejercicio 1: b)

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,2 +1,6 @@
 def nota_teoria(n1: float|None, n2:float|None) -> float:
     return ((n1 if type(n1) is float else 0) + (n2 if type(n2) is float else 0)) / 2
+
+def nota_cuatrimestre(ntt: tuple[float|None, float|None], np: float|None) -> float:
+    nt = nota_teoria(ntt[0], ntt[1])
+    return (0.2 * nt + 0.8 * (np if type(np) is float else 0)) if nt >= 4 else 0
--- src/calificaciones_test.py
+++ src/calificaciones_test.py
@@ -8,6 +8,14 @@ def test_nota_teoria():
     print(nota_teoria(9.0, None))
     print(nota_teoria(None, None))
 
-
+def test_nota_cuatrimestre():
+    print(nota_cuatrimestre((9.1, 7.2), 8.1))
+    print(nota_cuatrimestre((4.0, 6.0), 5.0))
+    print(nota_cuatrimestre((4.0, 3.0), None))
+    print(nota_cuatrimestre((None, 3.0), None))
+    print(nota_cuatrimestre((9.0, None), 4.5))
+    
+    
 if __name__ == '__main__':
-    test_nota_teoria()
+    #test_nota_teoria()
+    test_nota_cuatrimestre()
